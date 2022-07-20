434b01afb069f410ca14f5161378f10994017ab3
mkjaramillo <mkjaramillo@uce.edu.ec>
2022-07-19 23:37:06 -0500

    mapa realizado

--- app/src/main/java/com/example/proyecto_movil/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_movil/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.proyecto_movil
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.proyecto_movil.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId){
+
+                R.id.idMap->{
+                    startActivity(Intent(this,MapaActivity::class.java))
+                    true}
+                else->false
+
+
+            }
+        }
     }
 }
