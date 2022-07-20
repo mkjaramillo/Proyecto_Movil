0340266d2ffc5911d65213de13c5362b6f41c98e
Dallyana Vanegas <davanegas@uce.edu.ec>
2022-07-19 21:29:35 -0500

    by dallya ._.

--- app/src/main/java/com/example/proyecto_movil/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_movil/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.proyecto_movil
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
+import android.content.Intent
 import android.os.Bundle
 
+import androidx.appcompat.app.AppCompatActivity
+
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:com.example.proyecto_movil.databinding.ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        super.onCreate(savedInstanceState);
+
+        binding = com.example.proyecto_movil.databinding.ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ESTE.setOnClickListener {
+            startActivity(Intent(this, RegistroActivity::class.java))
+
+        }
     }
+
 }
